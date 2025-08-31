55dc9252058ecfecba5b0e8b202d99b309c29c4e
Yognda <117925895+GiovanniTortia@users.noreply.github.com>
2025-08-31 14:07:10 +0200

    added comments to main.py

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from hx711 import HX711
 import pages
 import _thread
 
-
+# SD card reader SPI setup
 cs = machine.Pin(13, machine.Pin.OUT)
-
 spi = machine.SPI(1,
                   baudrate = 1000000,
                   polarity=0,
@@ -27,6 +26,7 @@ uos.mount(vfs, '/sd')
 
 sensor = HX711(d_out=19, pd_sck=18)
 
+# Connects to wifi network ssid with password pw
 def connect(ssid, pw):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -42,35 +42,39 @@ Response.default_content_type = 'text/html'
 
 led=machine.Pin("LED", machine.Pin.OUT)
 
+# Homepage
 @app.route('/')
 def home(request):
     return pages.INDEX
 
+# Toggles the onboard led, used to check whether the device is responsive
 @app.route('/led', methods=['POST'])
 def led_toggle(request):
     led.toggle()
-    
+
+
+# The following 2 methods calibrate the sensor by computing its offset and gain
 @app.route('/tare', methods=['POST'])
 def tare_sensor(request):
     led.toggle()
-    print(sensor.tare())
+    sensor.tare()
     led.toggle()
-
+  
 @app.route('/setCF', methods=['POST'])
 def setCF(req):
     led.toggle()
     weight = float(req.form.get('weight'))
-    print('weight: ')
-    print(weight)
     CF = sensor.getCF(weight)
-    print(CF)
     led.toggle()
-    
+
+# Returns a single reading from the sensor
 @app.route('/read', methods=['GET'])
 def read(req):
     w = sensor.read()
     return {'weight':w}
 
+# When pressing the "start" button the raspberry starts reading values and logging them together with
+# the timestamp into the sd card for 30 seconds
 @app.route('/start', methods=['POST'])
 def thr_start(req):
     led.toggle()
@@ -86,10 +90,11 @@ def thr_start(req):
     
 
 if __name__ == '__main__':
-    ssid = 'Giova'
-    pw = 'horten229'
+    ssid = 'network_id'
+    pw = 'password'
     connect(ssid, pw)
     app.run(debug=True)
     
+
 
 
